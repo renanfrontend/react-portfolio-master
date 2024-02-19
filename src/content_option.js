6f262a2b430d1c10970cf0be35571a4ab6c97a15
renanfrontend <renan.santos_ext@olos.com.br>
2024-02-19 14:55:29 -0300

    upload project

---
+++ src/content_option.js
@@ -0,0 +1,166 @@
+const logotext = "RENAN";
+const meta = {
+    title: "Renan Augusto",
+    description: "Eu sou Renan Augusto, cientista de dados e desenvolvedor full stack, atualmente trabalhando em Berlin",
+};
+
+const introdata = {
+    title: "Eu sou Renan Augusto",
+    animated: {
+        first: "Eu amo programar",
+        second: "Eu crio websites incríveis",
+        third: "Eu desenvolvo Frontend com paixão",
+    },
+    description: "Olá! Meu nome é Renan Augusto e sou um desenvolvedor frontend apaixonado por criar experiências incríveis na web. Com habilidades em HTML, CSS e JavaScript, estou sempre em busca de aprimorar minhas habilidades e aprender novas tecnologias. Minha missão é transformar ideias em interfaces bonitas e funcionais, garantindo uma ótima experiência do usuário. Vamos trabalhar juntos para criar algo incrível!",
+    your_img_url: "./assets/images/background.jpg",
+};
+
+const dataabout = {
+    title: "Um pouco sobre mim",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+        jobtitle: "Designer da semana",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer da semana",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer da semana",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Django",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Design de UI e UX",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Aplicativos Mobile",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Design Wordpress",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "A sabedoria da vida consiste na eliminação do não essencial.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "A sabedoria da vida consiste na eliminação do não essencial.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "A sabedoria da vida consiste na eliminação do não essencial.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "A sabedoria da vida consiste na eliminação do não essencial.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "A sabedoria da vida consiste na eliminação do não essencial.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "A sabedoria da vida consiste na eliminação do não essencial.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "A sabedoria da vida consiste na eliminação do não essencial.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "A sabedoria da vida consiste na eliminação do não essencial.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "A sabedoria da vida consiste na eliminação do não essencial.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "A sabedoria da vida consiste na eliminação do não essencial.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "A sabedoria da vida consiste na eliminação do não essencial.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "A sabedoria da vida consiste na eliminação do não essencial.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "nome@dominio.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // crie uma conta no emailjs.com
+    // confira este tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
